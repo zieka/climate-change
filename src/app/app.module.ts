43117dd789d1aaac2cf397462de5d6eab09dfdce
Kyle Scully <scullykns@gmail.com>
2017-06-05 20:42:37 -0700

    added loading indicator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ClimateChangeTableComponent } from './climate-change-table/climate-change-table.component';
+import { LoadingIndicatorComponent } from './loading-indicator/loading-indicator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClimateChangeTableComponent
+    ClimateChangeTableComponent,
+    LoadingIndicatorComponent
   ],
   imports: [
     BrowserModule,
